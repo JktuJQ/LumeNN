4cd1705fcb97a543a8b118c853be03d9830ce452
JktuJQ <odmamontov@gmail.com>
2025-02-24 23:08:56 +0300

    First commit
    Created basic project structure ('main.py', 'README.md', '.gitignore' and 'LICENSE'). This is the first commit to this repository! 🥳🥳🥳

---
+++ README.md
@@ -0,0 +1,3 @@
+# LumeNN
+
+LumeNN is a neural network that is able to classify stars based on their various characteristics.
---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+import typing as t
+
+
+def main(argv: t.List[str]) -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main(sys.argv)
